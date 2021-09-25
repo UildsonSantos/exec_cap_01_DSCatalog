e24c0bda80f1076ec823d04225868d19c3176d5e
Uildson S. dos Santos <wilder_shon@hotmail.com>
2021-09-25 10:44:02 -0300

    Backend project created

---
+++ src/main/java/com/devsuperior/exec01/mydscatalog/MyDscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.exec01.mydscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDscatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/exec01/mydscatalog/MyDscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.exec01.mydscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
